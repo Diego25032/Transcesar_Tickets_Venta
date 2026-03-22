b5b7d9b8d5cec05499cd53b89086ac709b3ecd0b
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:18:57 -0500

    calcular descuento

--- Pasajero.java
+++ Pasajero.java
@@ -1,11 +1,17 @@
     import java.time.LocalDate;
 import java.time.Period;
 public abstract class Pasajero extends Persona {
-    public abstract double calcularDescuento();
-    
+
 protected LocalDate fechaNacimiento;
 
 public int calcularEdad() {
     return Period.between(fechaNacimiento, LocalDate.now()).getYears();
 }
+public double calcularDescuento() {
+    if (calcularEdad() >= 60) return 0.30;
+    return 0.0;
 }
+
+
+}
+

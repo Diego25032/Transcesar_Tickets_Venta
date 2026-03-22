00ea38cf8a326ef48d82d3bea00719956c135279
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:02:01 -0500

    override buseta

--- Buseta.java
+++ Buseta.java
@@ -2,4 +2,10 @@ public class Buseta extends Vehiculo {
     public Buseta(String placa, String ruta) {
         super(placa, ruta, 19);
     }
+
+    @Override
+    public void imprimirDetalle() {
+        // TODO Auto-generated method stub
+        
+    }
 }

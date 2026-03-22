c6be5f489db40758417fed92a6c5e5378f52ee55
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:46:37 -0500

    reservadao

--- ReservaDAO.java
+++ ReservaDAO.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReservaDAO {
-    public void guardar(Reserva r) { }
+
+    private List<Reserva> reservas = new ArrayList<>();
+
+    public void guardar(Reserva r) {
+        reservas.add(r);
+        System.out.println("Reserva guardada");
+    }
+
+    public List<Reserva> listar() {
+        return reservas;
+    }
 }

4f606c613e55e4d58f7abb1a8e5cdb5c5ab3325f
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:16:55 -0500

    modificado pasajero

--- Pasajero.java
+++ Pasajero.java
@@ -1,3 +1,11 @@
+    import java.time.LocalDate;
+import java.time.Period;
 public abstract class Pasajero extends Persona {
     public abstract double calcularDescuento();
+    
+protected LocalDate fechaNacimiento;
+
+public int calcularEdad() {
+    return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+}
 }

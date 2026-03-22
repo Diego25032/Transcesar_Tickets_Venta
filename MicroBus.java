d8b92bfcf64c97347511337eb13e997bfbe378f5
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:01:03 -0500

    overridemicrobus

--- MicroBus.java
+++ MicroBus.java
@@ -2,4 +2,10 @@ public class MicroBus extends Vehiculo {
     public MicroBus(String placa, String ruta) {
         super(placa, ruta, 25);
     }
+
+    @Override
+    public void imprimirDetalle() {
+        // TODO Auto-generated method stub
+        
+    }
 }

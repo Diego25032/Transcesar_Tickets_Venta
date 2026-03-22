1d9f6aa419c2a9f944238961010e115d79028cd9
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:45:56 -0500

    nuevos requerimientos clase reserva

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Reserva {
+
+    private String codigo;
     private Pasajero pasajero;
     private Vehiculo vehiculo;
-    private LocalDate fecha;
+    private LocalDateTime fechaCreacion;
+    private LocalDate fechaViaje;
+    private EstadoReserva estado;
+
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo, LocalDate fechaViaje) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaViaje = fechaViaje;
+        this.fechaCreacion = LocalDateTime.now();
+        this.estado = EstadoReserva.ACTIVA;
+    }
+
+    public boolean estaVencida() {
+        return fechaCreacion.plusHours(24).isBefore(LocalDateTime.now());
+    }
+
+    public void cancelar() {
+        estado = EstadoReserva.CANCELADA;
+    }
+
+    public void convertir() {
+        estado = EstadoReserva.CONVERTIDA;
+    }
+
+    // getters
 }

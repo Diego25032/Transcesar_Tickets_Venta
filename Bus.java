1d5514d819fa6cb100bbdf639ec003d909f2f908
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:02:53 -0500

    overridebus

--- Bus.java
+++ Bus.java
@@ -2,4 +2,10 @@ public class Bus extends Vehiculo {
     public Bus(String placa, String ruta) {
         super(placa, ruta, 45);
     }
+
+    @Override
+    public void imprimirDetalle() {
+        // TODO Auto-generated method stub
+        
+    }
 }

d82f8bcdc7847bcdc2cf688238de42b334fe376d
Diego Restrepo <direstrepo@gmail.com>
2026-03-21 23:15:43 -0500

    modificacion vehiculo

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,12 +1,11 @@
 public abstract class Vehiculo implements Imprimible {
     protected String placa;
-    protected String ruta;
+    protected Ruta ruta;
     protected int capacidadMaxima;
     protected int pasajerosActuales = 0;
 
     public Vehiculo(String placa, String ruta, int capacidadMaxima) {
         this.placa = placa;
-        this.ruta = ruta;
         this.capacidadMaxima = capacidadMaxima;
     }
 
